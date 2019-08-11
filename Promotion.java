f79ce7ca3d0dd28bdfc195fc5b8cb7269b0e6b94
karituason <karituason@users.noreply.github.com>
2019-08-11 17:59:34 -0400

    Add files via upload

---
+++ Promotion.java
@@ -0,0 +1,10 @@
+package assignment2;
+
+public class Promotion {
+	public static void main(String[] args) {
+		int i = 7;
+		System.out.println("Integer: " + i);
+		float l = i;
+		System.out.println("float l = i: " + l);
+	}
+}
